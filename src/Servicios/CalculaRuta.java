e84c6737fea406d6dd5a0fd510b121c5f4e0867f
fran <fran@email.com>
2025-03-05 13:07:52 +0100

    modificación

--- src/Servicios/CalculaRuta.java
+++ src/Servicios/CalculaRuta.java
@@ -4,17 +4,20 @@ import java.util.List;
 
 public class CalculaRuta {
 	public String rutaMasRapida(String origen, String destino) {
-		// Simulación de cálculo de ruta más rápida
-		return "Ruta 1";
+		if(!origen.isEmpty() && !origen.isEmpty())	
+			return "Ruta más rápida entre " +origen + " y " +destino;
+		return "Ruta no válida";
 	}
 
 	public String rutaMasEconomica(String origen, String destino, double presupuesto) {
 		// Simulación de cálculo de ruta más económica
+		//presupuesto>0
 		return "Ruta 2";
 	}
 
 	public String rutaConLugaresDeInteres(String origen, String destino, List<String> lugaresDeInteres) {
 	    // Simulación de cálculo de ruta con lugares de interés
+		//!lugaresDeInteres.isEmpty()
 	    return "Ruta 3";
 	}
 
